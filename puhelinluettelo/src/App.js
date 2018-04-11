b9fcf41f0a5f77fa5730223490b7a6234ef42872
Viljami <viljami.virolainen@aalto.fi>
2018-04-11 18:40:56 +0300

    did stuff

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -1,21 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      persons: [
+        { name: 'Arto Hellas' }
+      ],
+      newName: ''
+    }
+  }
+
+  addPerson = (event) => {
+    event.preventDefault()
+    
+    const personObject = {
+      name: this.state.newName
+    }
+    const names = this.state.persons.map((x)=>x.name)
+    if (!names.includes(personObject.name)) {
+      const persons = this.state.persons.concat(personObject)
+      this.setState({
+        persons: persons,
+        newName: ''
+      })
+    } else {
+      alert('Saman niminen henkilö on jo listalla')
+      this.setState({
+        persons: this.state.persons,
+        newName: ''
+      })
+    }
+    
+  }
+
+  handleNameChange = (event) => {
+    console.log(event.target.value)
+    this.setState({ newName: event.target.value })
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h2>Puhelinluettelo</h2>
+        <form onSubmit={this.addPerson}>
+          <div>
+            nimi: <input value={this.state.newName} onChange={this.handleNameChange}/>
+          </div>
+          <div>
+            <button type="submit">lisää</button>
+          </div>
+        </form>
+        <h2>Numerot</h2>
+        {this.state.persons.map((person)=><Person key={person.name} name={person.name}/>)}
       </div>
-    );
+    )
   }
 }
-
-export default App;
+const Person = ({name}) => {
+  return (<div>
+    {name}
+  </div>)
+}
+export default App
